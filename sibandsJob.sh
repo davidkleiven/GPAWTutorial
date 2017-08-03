591322f1ac6d98388176c841ef406a603bb01160
David Kleiven <davidkleiven446@gmail.com>
2017-08-03 17:23:26 +0200

    Added local bin to path

--- sibandsJob.sh
+++ sibandsJob.sh
@@ -9,6 +9,7 @@ module load python
 export GPAW_FFTWSO=''
 export LD_LIBRARY_PATH="/usr/lib64":"/home/ntnu/davidkl/.local/lib":${LD_LIBRARY_PATH}
 export GPAW_SETUP_PATH="/home/ntnu/davidkl/GPAW/gpawData/gpaw-setups-0.9.20000"
+export PATH=${PATH}:"/home/ntnu/davidkl/.local/bin"
 
 FOLDER="/home/ntnu/davidkl/GPAWTutorial"
 
