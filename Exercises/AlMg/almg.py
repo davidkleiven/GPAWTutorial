a4e2049f51d47dd6bb667b6870d8efc189bb2395
David Kleiven <davidkleiven446@gmail.com>
2017-08-17 14:21:56 +0200

    Minor change

--- Exercises/AlMg/almg.py
+++ Exercises/AlMg/almg.py
@@ -1,29 +1,46 @@
 import sys
+import os
 from ase import Atoms
 from ase.lattice.cubic import FaceCenteredCubic
 import ase.db
 import sqlite3 as sq
 from ase import build
 from gpaw.poisson import PoissonSolver
+import numpy as np
 
 def main( argv ):
     if ( len(argv) > 2 ):
         print ("Usage: python almg.py paramID")
         return
 
-    db_name = "/home/ntnu/davidkl/GPAWTutorial/Exercises/AlMg/AlMg.db"
+    possible_dbs = {
+        "vilje":"/home/ntnu/davidkl/GPAWTutorial/Exercises/AlMg/AlMg.db",
+        "laptop":"AlMg.db"
+    }
+
+    # Find which database to use
+    db_name = None
+    for key in possible_dbs:
+        if ( os.path.isfile(possible_dbs[key]) ):
+            db_name = possible_dbs[key]
+            break
+    if ( db_name is None ):
+        raise RuntimeError("Could not find database")
+
+    #db_name = "/home/ntnu/davidkl/GPAWTutorial/Exercises/AlMg/AlMg.db"
     db = ase.db.connect( db_name )
     runID = int(argv[0])
 
     # Read parameters from the database
     con  = sq.connect( db_name )
     cur = con.cursor()
-    cur.execute( "SELECT hspacing,relax,atomID,kpts,nbands FROM runs WHERE ID=?", (runID,) )
+    cur.execute( "SELECT hspacing,relax,atomID,kpts,nbands,latticeConst FROM runs WHERE ID=?", (runID,) )
     params = cur.fetchall()[0]
     con.close()
 
     save_pov = False
     run_sim = True
+    swap_atoms = False
     h_spacing = params[0]
     relax = params[1]
     atom_row_id = params[2]
@@ -36,18 +53,15 @@ def main( argv ):
     NatomsZ = 4
 
     # Lattice parameter
-    a = 4.05
+    a = float( params[5] )
 
     if ( atom_row_id < 0 ):
         # Target primitive cell
-        atoms = build.bulk( "Al", crystalstructure="fcc" )
+        atoms = build.bulk( "Al", crystalstructure="fcc", a=a )
 
         # Create a supercell consisting of 32 atoms
         P = build.find_optimal_cell_shape_pure_python( atoms.cell, 32, "sc" )
         atoms = build.make_supercell( atoms, P )
-        print (len(atoms))
-
-        print ("Number of atoms: %d"%( len(atoms)) )
 
         # Replace some atoms with Mg atoms
         n_mg_atoms = int( 0.2*len(atoms) )
@@ -58,9 +72,20 @@ def main( argv ):
         # Read atoms from database
         atoms = db.get_atoms( selection=atom_row_id )
 
+    if ( swap_atoms ):
+        from ase.io import write
+        for i in range(0, 2*len(atoms) ):
+            first = np.random.randint(0,len(atoms))
+            second = np.random.randint(0,len(atoms))
+            firstSymbol = atoms[first].symbol
+            atoms[first].symbol = atoms[second].symbol
+            atoms[second].symbol = firstSymbol
+        atoms.write( "almg_swap.xyz" )
+
     if ( save_pov ):
         from ase.io import write
         write( "Al.pov", atoms*(3,3,1), rotation="-10z,-70x" )
+        return
 
     if ( run_sim ):
         from gpaw import GPAW
