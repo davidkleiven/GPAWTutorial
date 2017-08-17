72c6b08a08c533ab29b3e6a015702be95d4ad547
David Kleiven <davidkleiven446@gmail.com>
2017-08-17 15:12:03 +0200

    Minor change

--- Exercises/AlMg/almg.py
+++ Exercises/AlMg/almg.py
@@ -98,7 +98,7 @@ def main( argv ):
         else:
             mode = "fd"
         #calc = GPAW( mode=mode, h=h_spacing, xc="PBE", nbands=nbands, kpts=kpts, basis="dzp")#, poissonsolver=PoissonSolver(relax="GS", eps=1E-7) )
-        calc = GPAW( mode=PW(cutoff), xc="PBE", nbands=nbands, kpts=kpts )
+        calc = GPAW( mode=PW(cutoff), h=h_spacing, xc="PBE", nbands=nbands, kpts=kpts )
         atoms.set_calculator( calc )
 
         if ( relax ):
