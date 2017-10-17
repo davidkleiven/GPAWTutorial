c56f64d89f989d9263bd9c282083a6e0edbee594
David Kleiven <david.kleiven@ntnu.no>
2017-10-17 14:03:48 +0200

    Use fd mode

--- Scaling/dftRuns.py
+++ Scaling/dftRuns.py
@@ -7,12 +7,19 @@ from ase.constraints import UnitCellFilter
 from ase.io.trajectory import Trajectory
 from ase.build import bulk
 import os
+from gpaw.utilities.tool import cutoff2gridspacing
 
 def main( argv ):
     atoms = bulk("Al")
     atoms = atoms*(4,4,4)
+    mode = "fd"
 
-    calc = gp.GPAW( mode=gp.PW(500), xc="PBE", kpts=(6,6,6), nbands=-10 )
+    e_cut = 500
+    if ( mode == "fd" ):
+        h = cutoff2gridspacing(e_cut)
+        calc = gp.GPAW(mode="fd", h=h, xc="PBE", kpts=(6,6,6), nbands=-10 )
+    else:
+        calc = gp.GPAW( mode=gp.PW(e_cuts), xc="PBE", kpts=(6,6,6), nbands=-10 )
     atoms.set_calculator( calc )
 
     energy = atoms.get_potential_energy()
