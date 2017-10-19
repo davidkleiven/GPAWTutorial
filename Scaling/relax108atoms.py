052aefc9fc78e43a590728cf7f7a07778d306695
David Kleiven <david.kleiven@ntnu.no>
2017-10-19 15:28:07 +0200

    Try to relax a 108 atom structure

---
+++ Scaling/relax108atoms.py
@@ -0,0 +1,37 @@
+import sys
+import gpaw as gp
+from ase.optimize.precon import PreconLBFGS
+from ase.optimize.precon.precon import Exp
+from ase.io.trajectory import Trajectory
+from ase.build import bulk
+from ase.visualize import view
+
+def main( argv ):
+    atoms = bulk("Al",cubic=True)
+    atoms = atoms*(3,3,3)
+    for i in range(int(len(atoms)/5)):
+        atoms[i].symbol = "Mg"
+
+    atoms.rattle( stdev=0.005 )
+
+    parallel = {
+        "order":"dkb",
+        "domain":(4,4,4)
+    }
+    calc = gp.GPAW( mode="fd", h=0.2, xc="PBE", kpts=(3,3,3), nbands="120%", parallel=parallel )
+    atoms.set_calculator( calc )
+
+    logfile = "relax250.log"
+    traj = "relax250.traj"
+    trajObj = Trajectory(traj, 'w', atoms )
+
+    precon = Exp(mu=1)
+    relaxer = PreconLBFGS( atoms, logfile=logfile, use_armijo=True, precon=precon )
+    relaxer.attach( trajObj )
+    try:
+        relaxer.run( fmax=0.05 )
+    except:
+        pass
+
+if __name__ == "__main__":
+    main( sys.argv[1:] )
