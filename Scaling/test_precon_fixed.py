4248925bad2cc217bdb9ee3dfcc188320bb2e7fa
David Kleiven <david.kleiven@ntnu.no>
2017-10-19 09:15:47 +0200

    Precon with fixed preconditioner

---
+++ Scaling/test_precon_fixed.py
@@ -0,0 +1,34 @@
+import sys
+import gpaw as gp
+from ase.optimize.precon import PreconLBFGS
+from ase.optimize.precon.precon import Exp
+from ase.io.trajectory import Trajectory
+from ase.build import bulk
+
+def main( argv ):
+    n_mg = int(argv[0])
+    atoms = bulk("Al")
+    atoms = atoms*(4,4,4)
+    for i in range(n_mg):
+        atoms[i].symbol = "Mg"
+
+    atoms.rattle( stdev=0.005 )
+
+    calc = gp.GPAW( mode=gp.PW(500), xc="PBE", kpts=(4,4,4), nbands="120%" )
+    atoms.set_calculator( calc )
+
+    logfile = "preconTest%d.log"%(n_mg)
+    traj = "preconTest%d.traj"%(n_mg)
+    trajObj = Trajectory(traj, 'w', atoms )
+
+    precon = Exp(mu=1)
+    relaxer = PreconLBFGS( atoms, logfile=logfile, use_armijo=True, precon=precon )
+    relaxer.attach( trajObj )
+    try:
+        relaxer.run( fmax=0.05 )
+    except:
+        pass
+    print ("Mu: %.2E"%(relaxer.precon.mu))
+
+if __name__ == "__main__":
+    main( sys.argv[1:] )
