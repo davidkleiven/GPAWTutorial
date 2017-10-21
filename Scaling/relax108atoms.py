67b1af77a8f0c9141a60e217a8385c4317635ea6
David Kleiven <davidkleiven446@gmail.com>
2017-10-21 10:54:37 +0200

    Try to reduce the number of steps stored in the preconditoner

--- Scaling/relax108atoms.py
+++ Scaling/relax108atoms.py
@@ -23,7 +23,7 @@ def main():
     trajObj = Trajectory(traj, 'w', atoms )
 
     precon = Exp(mu=1)
-    relaxer = PreconLBFGS( atoms, logfile=logfile, use_armijo=True, precon=precon )
+    relaxer = PreconLBFGS( atoms, logfile=logfile, use_armijo=True, precon=precon, memory=50 )
     relaxer.attach( trajObj )
     try:
         relaxer.run( fmax=0.05 )
