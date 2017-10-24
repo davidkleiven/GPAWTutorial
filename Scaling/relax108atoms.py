6ace4999e78b083118a2098f41bf12152dd41d7a
David Kleiven <david.kleiven@ntnu.no>
2017-10-24 09:16:07 +0200

    Relax 512 atoms

--- Scaling/relax108atoms.py
+++ Scaling/relax108atoms.py
@@ -8,14 +8,14 @@ from ase.visualize import view
 
 def main():
     atoms = bulk("Al")
-    atoms = atoms*(6,6,6)
+    atoms = atoms*(8,8,8)
     for i in range(int(len(atoms)/5)):
         atoms[i].symbol = "Mg"
 
 
     atoms.rattle( stdev=0.005 )
 
-    calc = gp.GPAW( mode="fd", h=0.2, xc="PBE", kpts=(2,2,2), nbands="120%" )
+    calc = gp.GPAW( mode="fd", h=0.2, xc="PBE", kpts=(1,1,1), nbands="120%" )
     atoms.set_calculator( calc )
 
     logfile = "relax250.log"
