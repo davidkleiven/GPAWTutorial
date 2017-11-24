73f96f8f0a48531bebf42b3568a97c9e6100b3d5
David Kleiven <david.kleiven@ntnu.no>
2017-11-24 17:21:08 +0100

    Have to propose a different element

--- CE_extensions/wang_landau_scg.py
+++ CE_extensions/wang_landau_scg.py
@@ -4,10 +4,11 @@ import pickle as pkl
 import ase.units as units
 from matplotlib import pyplot as plt
 from scipy import interpolate
+import copy
 
 class WangLandauSGC( object ):
     def __init__( self, atoms, calc, chemical_potentials={}, site_types=None, site_elements=None, Nbins=100, initial_f=2.71,
-    flatness_criteria=0.8, fmin=1E-8, Emin=0.0, Emax=1.0 ):
+    flatness_criteria=0.8, fmin=1E-8, Emin=None, Emax=None ):
         self.atoms = atoms
         self.site_types = site_types
         self.site_elements = site_elements
@@ -16,8 +17,9 @@ class WangLandauSGC( object ):
         self.dos = np.zeros(Nbins)
         self.entropy = np.zeros(Nbins)
         self.Nbins = Nbins
-        self.Emin = Emin
-        self.Emax = Emax
+        self.Emin = 0.0
+        self.Emax = 1.0
+        self.update_bounds_every = 100
         self.E = np.linspace(self.Emin, self.Emax, self.Nbins )
         self.f = initial_f
         self.f0 = initial_f
@@ -27,6 +29,7 @@ class WangLandauSGC( object ):
         self.calc = calc
         self.current_bin = 0
         self.fmin = 1E-8
+        self.structures = [None for _ in range(self.Nbins)]
 
         if (len(self.atoms) != len(self.site_types )):
             raise ValueError( "A site type for each site has to be specified!" )
@@ -60,10 +63,10 @@ class WangLandauSGC( object ):
                 self.atoms_count[atom.symbol] = 1
 
     def get_bin( self, energy ):
-        return int( (energy-self.Emin)*(self.Nbins-1)/(self.Emax-self.Emin) )
+        return int( (energy-self.Emin)*self.Nbins/(self.Emax-self.Emin) )
 
     def get_energy( self, indx ):
-        return self.Emin + (self.Emax-self.Emin )*indx/(self.Nbins-1)
+        return self.Emin + (self.Emax-self.Emin )*indx/self.Nbins
 
     def _step( self ):
         """
@@ -74,23 +77,24 @@ class WangLandauSGC( object ):
 
         site_type = self.site_types[indx]
         possible_elements = self.site_elements[site_type]
-        new_symbol = possible_elements[np.random.randint(low=0,high=len(possible_elements))]
+        new_symbol = symb
+        # This is slow and should be optimized
+        while ( new_symbol == symb ):
+            new_symbol = possible_elements[np.random.randint(low=0,high=len(possible_elements))]
         system_changes = [(indx,new_symbol)]
+        self.atoms[indx].symbol = new_symbol
         energy = self.atoms.get_potential_energy()
         #self.calc.calculate( self.atoms, properties=["energy"], system_changes=system_changes )
         chem_pot_change = self.chem_pot[symb]*(self.atoms_count[symb]-1) + self.chem_pot[new_symbol]*(self.atoms_count[new_symbol]+1)
         #energy = self.calc.results["energy"]-chem_pot_change
         energy -= chem_pot_change
+        #selected_bin = self.get_bin(energy)
+
+        if ( energy < self.Emin ):
+            self.redistribute_hist(energy,self.Emax)
+        elif ( energy >= self.Emax ):
+            self.redistribute_hist(self.Emin,energy)
         selected_bin = self.get_bin(energy)
-
-
-        bin_in_range = (selected_bin >= 0 ) and ( selected_bin < self.Nbins )
-        if ( not bin_in_range ):
-            if ( energy < self.Emin ):
-                self.redistribute_hist(energy,self.Emax)
-            else:
-                self.redistribute_hist(self.Emin,energy)
-            selected_bin = self.get_bin(energy)
 
         rand_num = np.random.rand()
         diff = self.entropy[self.current_bin]-self.entropy[selected_bin]
@@ -103,6 +107,9 @@ class WangLandauSGC( object ):
             self.atoms_count[symb] -= 1
             self.atoms_count[new_symbol] += 1
             self.atoms[indx].symbol = new_symbol
+
+            # Store this object
+            self.structures[selected_bin] = self.atoms.copy()
         else:
             self.atoms[indx].symbol = symb
 
@@ -110,12 +117,42 @@ class WangLandauSGC( object ):
         self.entropy[self.current_bin] += self.f
 
     def is_flat( self ):
-        mean = np.mean( self.histogram )
-        return np.min(self.histogram) > self.flatness_criteria*mean
+        if ( np.sum(self.histogram) < 100 ):
+            return False
+
+        mask = np.zeros(len(self.histogram),dtype=np.int8)
+        mask[:] = True
+        for i in range(len(self.histogram)):
+            if ( self.histogram[i] == 0 and self.structures[i] is None ):
+                mask[i] = False
+        mean = np.mean( self.histogram[mask] )
+        #return np.percentile(self.histogram, 10 ) > self.flatness_criteria*mean
+        return np.min(self.histogram[mask]) > self.flatness_criteria*mean
 
     def save( self, fname ):
         with open(fname,'wb') as ofile:
             pkl.dump(self,ofile)
+
+    def set_number_of_bins( self, N ):
+        self.Nbins = N
+        self.redistribute_hist(self.Emin,self.Emax)
+
+    def goto_lowest_entropy_structure( self ):
+        """
+        Change to system to one of the structures that are known to be in the
+        low density region
+        """
+        args = np.argsort(self.histogram)
+        for i in range(len(args) ):
+            if ( self.structures[args[i]] is None ):
+                continue
+            self.atoms = self.structures[args[i]]
+            self.atoms.set_calculator( self.calc )
+            self.current_bin = args[i]
+            for key in self.atoms_count.keys():
+                self.atoms_count[key]=0
+            self.initialize() # Update the atoms count
+            return
 
     def update_range( self ):
         """
@@ -141,6 +178,9 @@ class WangLandauSGC( object ):
         """
         Redistributes the histograms
         """
+        eps = 1E-8
+        Emax += eps
+        old_energy = self.get_energy(self.current_bin)
         new_E = np.linspace( Emin, Emax, self.Nbins )
         interp_hist = interpolate.interp1d( self.E, self.histogram, bounds_error=False, fill_value=0 )
         new_hist = interp_hist(new_E)
@@ -152,43 +192,45 @@ class WangLandauSGC( object ):
             new_hist *= np.sum(self.histogram)/np.sum(new_hist)
         if ( np.sum(new_logdos) > 0 ):
             new_logdos *= np.sum(self.entropy)/np.sum(new_logdos)
+        old_energies = copy.deepcopy(self.E)
         self.E = new_E
-        self.histogram = new_hist.astype(np.int32)
+        self.histogram = np.floor(new_hist).astype(np.int32)
         self.entropy = new_logdos
         self.Emin = Emin
         self.Emax = Emax
-
-    def optimize_bins( self ):
-        """
-        Optimize the binning to have more equal number of visits
-        """
-        mean_visits = np.mean(self.histogram)
-        bins = []
-        for i in range(0,len(self.histogram)):
-            n_splits = int(np.log2(self.histogram[i]/mean_visits))
-
+        new_structs = [None for _ in range(self.Nbins)]
+        for i in range(len(old_energies)):
+            new_indx = self.get_bin(old_energies[i])
+            if ( new_indx < 0 or new_indx >= self.Nbins ):
+                continue
+            new_structs[new_indx] = self.structures[i]
+        self.structures = new_structs
+        self.current_bin = self.get_bin(old_energy)
 
     def run( self, maxsteps=10000000 ):
         f_small_enough = False
-        update_bounds_every = 30
+        low_struct = 200
         for i in range(maxsteps):
             self._step()
-            if ( i%update_bounds_every == 0 and i > 0 and self.f > 0.5*self.f0 ):
+            if ( i%self.update_bounds_every == 0 and i > 0 and self.f > 0.5*self.f0 ):
                 self.update_range()
 
             if ( self.is_flat() ):
                 self.histogram[:] = 0
                 self.f *= 0.5
+                self.entropy -= np.max(self.entropy)
+                self.entropy[self.entropy<-40] = -40 # Reset ti a vanishingly small number
+
+            if ( i%low_struct == 0 and i > 0 ):
+                print ("Resetting to a low entropy structure")
+                self.goto_lowest_entropy_structure()
 
             if ( self.f < self.fmin ):
                 f_small_enough = True
                 break
-
-        # Avoid overflow because large entropy, scale down by a factor so subtract of the mean
-        # before exponentiating
-        print ("Current f: {}".format(self.f))
         self.dos = np.exp(self.entropy - np.mean(self.entropy))
-        #self.dos = np.exp(self.entropy)
+        print ("Current f: {}".format(self.f))
+        print (self.structures)
 
     def sgc_partition_function( self, T ):
         """
@@ -197,13 +239,14 @@ class WangLandauSGC( object ):
         return np.sum( self.dos*self._boltzmann_factor(T) )
 
     def _boltzmann_factor( self, T ):
-        return np.exp( -self.E/(units.kB*T) )
+        E0 = np.min(self.E)
+        return np.exp( -(self.E-E0)/(units.kB*T) )
 
     def sgc_avg_energy( self, T ):
         """
         Computes the average energy in the SGC ensemble
         """
-        return np.sum( self.E*self.dos*self._boltzmann_factor )/self.sgc_partition_function(T)
+        return np.sum( self.E*self.dos*self._boltzmann_factor(T) )/self.sgc_partition_function(T)
 
     def sgc_heat_capacity( self, T ):
         """
@@ -224,4 +267,10 @@ class WangLandauSGC( object ):
         ax = fig.add_subplot(1,1,1)
         ax.plot( self.E, self.dos, ls="steps" )
         ax.set_yscale("log")
+        return fig
+
+    def plot_histogram( self ):
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        ax.plot( self.E, self.histogram, ls="steps" )
         return fig
