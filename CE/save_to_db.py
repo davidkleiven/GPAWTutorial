84ecc09cf8b9975d331bc8b1ac33f529ca69d72b
David Kleiven <david.kleiven@ntnu.no>
2018-01-16 09:11:30 +0100

    Store the max and min stress if they are not updated

--- CE/save_to_db.py
+++ CE/save_to_db.py
@@ -1,11 +1,18 @@
 import ase.db
 class SaveToDB(object):
-    def __init__(self, db_name, runID, name):
+    def __init__(self, db_name, runID, name, mode="both"):
+        allowed_modes = ["both","cell","positions"]
+        if ( not mode in allowed_modes ):
+            raise ValueError( "Mode has to be one of {}".format(allowed_modes) )
         self.db = ase.db.connect( db_name )
         self.runID = runID
         self.name = name
         self.smallestEnergy = 1000.0
         self._is_first = True
+        row = self.db.get( id=runID )
+        self.fmax = row.get( "fmax", default=None )
+        self.smax = row.get( "smax", default=None )
+        self.mode = mode
 
     def __call__(self, atoms=None):
         """
@@ -23,3 +30,10 @@ class SaveToDB(object):
             key_value_pairs = self.db.get(name=self.name).key_value_pairs
             del self.db[self.runID]
             self.runID = self.db.write( atoms, key_value_pairs=key_value_pairs )
+
+            if ( (mode == "cell") and (not self.fmax is None) ):
+                # Keep the old fmax component if the object relaxes the cell
+                self.db.update(id=self.runID,max_force=self.fmax )
+            elif ( (mode == "positions") and  (not self.smax is None) ):
+                # Store the old maximum stress
+                self.db.update(id=self.runID,max_stress=self.smax)
