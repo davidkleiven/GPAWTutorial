7087de881123f81cc5e4841326aaa7b550324423
David Kleiven <david.kleiven@ntnu.no>
2018-02-21 12:21:17 +0100

    Variable cell from commandline

--- CE/relax_al3mg2.py
+++ CE/relax_al3mg2.py
@@ -6,15 +6,23 @@ from ase.calculators.emt import EMT
 import gpaw as gp
 from ase.build import bulk
 
-def main( fname ):
+def main( argv ):
+    fname = argv[0]
+    if ( int(argv[1]) == 1 ):
+        variable_cell = True
+    else:
+        variable_cell = False
     atoms = read( fname )
     #atoms = bulk("Al")
 
     calc = gp.GPAW( mode=gp.PW(500), kpts=(12,12,12), xc="PBE", nbands=-20 )
     atoms.set_calculator( calc )
     prc = Exp(mu=1.0,mu_c=1.0)
-    relaxer = PreconLBFGS( atoms, logfile="al12mg17.log", precon=prc, use_armijo=True, variable_cell=True )
-    relaxer.run( fmax=0.025, smax=0.003 )
+    relaxer = PreconLBFGS( atoms, logfile="al3mg2.log", precon=prc, use_armijo=True, variable_cell=variable_cell )
+    if ( variable_cell ):
+        relaxer.run( fmax=0.025, smax=0.003 )
+    else:
+        relaxer.run( fmax=0.025 )
     outfname = fname.split("/")[-1]
     outfname = outfname.split(".")[0]
     outfname += "_relaxed.xyz"
@@ -22,4 +30,4 @@ def main( fname ):
     write( outfname, atoms )
 
 if __name__ == "__main__":
-    main( sys.argv[1] )
+    main( sys.argv[1:] )
