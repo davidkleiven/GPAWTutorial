e2806d606c6e77d5af5e8da7376ee9f5751a0b14
David Kleiven <david.kleiven@ntnu.no>
2018-05-23 10:32:20 +0200

    Script for extracting final data form cell files

---
+++ CE/extract_statistics_gpw.py
@@ -0,0 +1,25 @@
+import glob
+import json
+
+def extract(fname):
+    atoms, calc = gp.restart(fname){}
+    result["cell"] = atoms.get_cell().tolist()
+    result["cellpar"] = atoms.get_cell_lengths_and_angles().tolist()
+    return result
+
+def main():
+    all_res = {}
+    outfname = "almgsi_cellpar.json"
+    for fname in glob.glob("/global/work/davidkl/AlMgSi/*.gpw"):
+        try:
+            res = extract(fname)
+            all_res[fname] = res
+        except Exception as exc:
+            print (str(exc))
+
+    with open( outfname, 'w' ) as outfile:
+        json.dump( all_res, outfile, indent=2, separators=(",",":") )
+    print ("Data written to {}".format(outfname))
+
+if __name__ == "__main__":
+    main()
