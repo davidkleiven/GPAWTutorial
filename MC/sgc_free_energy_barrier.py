caee70d6d64e9f076680c15996792e7f4486bd48
David Kleiven <davidkleiven446@gmail.com>
2018-05-26 15:40:53 +0200

    Small change

--- MC/sgc_free_energy_barrier.py
+++ MC/sgc_free_energy_barrier.py
@@ -36,7 +36,6 @@ def main():
     ceBulk = calc.BC
     ceBulk.atoms.set_calculator( calc )
 
-    chem_pot = {"c1_0":-1.0651526881167124}
     chem_pot = {"c1_0":-1.069}
 
     T = 300
@@ -44,25 +43,6 @@ def main():
     n_windows=20, n_bins=10, min_singlet=0.5, max_singlet=1.0, mpicomm=comm )
     mc.run( nsteps=100000, chem_pot=chem_pot )
     mc.save( fname="data/free_energy_barrier.json" )
-    #mc.plot( fname="data/free_energy_barrier.json" )
-    #plt.show()
-
-
-def plot(fname):
-    with h5.File(fname,'r') as hf:
-        hist = np.array( hf["overall_hist"])
-
-    beta_G = -np.log(hist)
-    beta_G -= beta_G[0]
-
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.plot( beta_G, ls="steps" )
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
-    ax.set_xlabel( "Number of Mg atoms" )
-    ax.set_ylabel( "\$\\beta \Delta G\$" )
-    plt.show()
 
 if __name__ == "__main__":
     main()
