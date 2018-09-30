6c514cb96d19e59c845a80b0ed3cf3a2113fc9b8
David Kleiven <davidkleiven446@gmail.com>
2018-09-30 13:22:47 +0200

    Plot the energy evolution

---
+++ AlMgSiMC/plot_energy_evolution.py
@@ -0,0 +1,22 @@
+import dill
+from cemc.mcmc import Montecarlo
+from matplotlib import pyplot as plt
+
+def plot_evolution(fname):
+    mc = Montecarlo.load(fname)
+    energy_obs = None
+    for obs in mc.observers:
+        if obs.name == "EnergyEvolution":
+            energy_obs = obs
+
+    if energy_obs is None:
+        raise RuntimeError("Did not find an energy evolution observer!")
+
+    energy = energy_obs.energies
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.plot(energy)
+    plt.show()
+
+
+plot_evolution("data/large_cluster/mc_backup400.pkl")
