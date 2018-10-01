70d753789765f1016fa33247d1d9fb298490cff4
David Kleiven <davidkleiven446@gmail.com>
2018-10-01 11:32:49 +0200

    Added DFT script for real space formulation

--- CE/AlMgSiX_FCC/almgsiXfcc_ce.py
+++ CE/AlMgSiX_FCC/almgsiXfcc_ce.py
@@ -1,7 +1,8 @@
 import sys
-from ase.ce import BulkCrystal
+from ase.ce import BulkCrystal, GenerateStructures
 
 db_name = "almgsiX_fcc.db"
+eci_fname = "data/eci_almgsix.json"
 
 def main(argv):
     option = argv[0]
@@ -13,11 +14,14 @@ def main(argv):
     }
     N = 6
 
-    ceBulk = BulkCrystal(crystalstructure="fcc", a=4.05, size=[N,N,N], basis_elements=[["Al","Mg","Si", "X"]], \
-    conc_args=conc_args, db_name=db_name, max_cluster_size=4)
+    ceBulk = BulkCrystal(crystalstructure="fcc", a=4.05, size=[N,N,N], basis_elements=[["Al", "Mg", "Si", "X"]], \
+    conc_args=conc_args, db_name=db_name, max_cluster_size=4, max_cluster_dia=[0.0, 0.0, 5.0, 4.1, 4.1])
 
+    struc_generator = GenerateStructures( ceBulk, struct_per_gen=10 )
     if option == "reconfig_settings":
         ceBulk.reconfigure_settings()
-
+    elif option == "insert":
+        fname = argv[1]
+        struc_generator.insert_structure(init_struct=fname)
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ CE/AlMgSiX_FCC/dft_almgsiX.py
@@ -0,0 +1,35 @@
+import gpaw as gp
+from ase.db import connect
+
+db_name = "/home/davidkl/GPAWTutorial/CE/AlMgSiX_FCC/almgsiX_fcc.db"
+
+def main(argv):
+    uid = int(argv[0])
+    kpts_density = float(argv[1])
+    lattice_param = float(argv[2])
+    relax_atoms = int(argv[3])
+    final_structure = int(argv[4])
+
+    db = connect(db_name)
+    atoms = db.get(id=uid).toatoms()
+    name = db.get(id=uid).name
+
+    # Scale the volume
+    pos_scaling = lattice_param/4.05
+    cell = atoms.get_cell()
+    cell *= pos_scaling
+    atoms.set_cell(cell, scale_atoms=True)
+
+    kpt = {"density": kpts_density, "even": True}
+    calc = gp.GPAW(h=0.18, kpt=kpt, xc="PBE", nbands="120%")
+    atoms.set_calculator(calc)
+
+    if relax_atoms == 0 and final_structure == 0:
+        atoms.get_potential_energy()
+
+        # Store the energy of the atoms object with the correct name
+        # and lattice parameter
+        db.write(atoms, name=name, lattice_param=lattice_param)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
