82511576f06bd436891e23bd172f5c11e46b8306
David Kleiven <davidkl@sophus.phys.ntnu.no>
2018-10-01 09:57:06 +0200

    Small change

--- CE/AlMgSiX_FCC/grid_space_convergence.py
+++ CE/AlMgSiX_FCC/grid_space_convergence.py
@@ -3,15 +3,15 @@ from ase.db import connect
 from ase.build import bulk
 import sys
 
-DB_NAME = "/home/davidkl/GPAWTutorial/CE/AlMgSiX/grid_space_conv.db"
+DB_NAME = "/home/davidkl/GPAWTutorial/CE/AlMgSiX_FCC/grid_space_conv.db"
 
 def run(h, symb):
     atoms = bulk("Al")
     atoms[0].symbol = symb
 
     kpts = {"density": 5.4, "even": True}
-    calc = GPAW(h=h, kpts=kpts, nbands=-100)
-    atoms.set_calculator()
+    calc = gp.GPAW(h=h, kpts=kpts, nbands=-100)
+    atoms.set_calculator(calc)
     atoms.get_potential_energy()
 
     db = connect(DB_NAME)
