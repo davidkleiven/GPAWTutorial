e10d185accb1f6a0980830e8bdb0fd5c3436f7fb
David Kleiven <davidkleiven446@gmail.com>
2018-10-01 10:50:51 +0200

    Cluster expansion script for Al-Mg-Si-X

---
+++ CE/AlMgSiX_FCC/almgsiXfcc_ce.py
@@ -0,0 +1,23 @@
+import sys
+from ase.ce import BulkCrystal
+
+db_name = "almgsiX_fcc.db"
+
+def main(argv):
+    option = argv[0]
+    conc_args = {
+        "conc_ratio_min_1":[[64,0,0]],
+        "conc_ratio_max_1":[[24,40,0]],
+        "conc_ratio_min_2":[[64,0,0]],
+        "conc_ratio_max_2":[[22,21,21]]
+    }
+    N = 6
+
+    ceBulk = BulkCrystal(crystalstructure="fcc", a=4.05, size=[N,N,N], basis_elements=[["Al","Mg","Si", "X"]], \
+    conc_args=conc_args, db_name=db_name, max_cluster_size=4)
+
+    if option == "reconfig_settings":
+        ceBulk.reconfigure_settings()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
