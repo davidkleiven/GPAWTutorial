39febc2873be0acfe35f72edd5eadcc243a5b2e3
David Kleiven <davidkleiven446@gmail.com>
2018-11-29 17:53:39 +0100

    Storing result

--- AlMgSiMC/dft_mgsi100_elastic.py
+++ AlMgSiMC/dft_mgsi100_elastic.py
@@ -1,8 +1,10 @@
 from gpaw import GPAW
 from gpaw import PW
-from ase.io import read
+from ase.io import read, write
 from ase.optimize.precon import PreconFIRE
+from ase.db import connect
 
+db_name = "/home/davidkl/GPAWTutorial/AlMgSiMC/mgsi100_elastic.db"
 def relax():
     fname = "/global/work/davidkl/MgSi100/mgsi_100.xyz"
     atoms = read(fname)
@@ -11,7 +13,15 @@ def relax():
     atoms.set_calculator(calc)
     relaxer = PreconFIRE(atoms, logfile="mgsi100_relax.log")
     relaxer.run(fmax=0.025, smax=0.003)
+    write("mgsi100_fully_relaxed.xyz", atoms)
 
+def single_point_energy(uid):
+    db = connect(db_name)
+    atoms = db.get(id=uid).toatoms()
+
+    calc = GPAW(mode=PW(800), xc="PBE", nbands=-60, kpts={"density": 5.4, "even": True})
+    stress = atoms.get_stress()
+    db.write(atoms, init_struct=uid)
 
 if __name__ == "__main__":
     relax()
