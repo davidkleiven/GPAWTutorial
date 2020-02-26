167c4f144fd52b0cfe31d78896cf2261b15c7865
David Kleiven <davidkleiven446@gmail.com>
2020-02-26 18:02:20 +0100

    Update redundant element

--- CE/AlMgSiX_FCC/reconfigure_database.py
+++ CE/AlMgSiX_FCC/reconfigure_database.py
@@ -7,5 +7,7 @@ setting = settingFromJSON("almgsixSettings3.json")
 db = connect(setting.db_name)
 db.delete_external_table('polynomial_cf')
 db.delete_external_table('binary_linear_cf')
+bf = BinaryLinear(setting.unique_elements, reduntant_element='X')
+setting.basis_func_type = bf
 cf = CorrFunction(setting)
 cf.reconfigure_db_entries()
